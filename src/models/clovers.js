bdadd8b3136a0cd26d3ce8ab8429a6868927018f
N Kort <nics@limnerstudio.co.uk>
2018-08-01 20:02:18 +0200

    store clover price in db

--- src/models/clovers.js
+++ src/models/clovers.js
@@ -1,27 +1,30 @@
-import r from "rethinkdb";
-import { events } from "../lib/ethers-utils";
-import { sym } from "../lib/util";
+import r from "rethinkdb"
+import { events } from "../lib/ethers-utils"
+import { sym } from "../lib/util"
 
 export const cloversTransfer = ({ log, io, db }) => {
   return Promise.all([
     new Promise(async (resolve, reject) => {
       if (log.data._from === "0x0000000000000000000000000000000000000000") {
         try {
-          var cloverMoves = await events.Clovers.instance.getCloverMoves(
+          let cloverMoves = await events.Clovers.instance.getCloverMoves(
             log.data._tokenId
-          );
-          var cloverReward = await events.Clovers.instance.getReward(
+          )
+          let cloverReward = await events.Clovers.instance.getReward(
             log.data._tokenId
-          );
-          var cloverSymmetries = await events.Clovers.instance.getSymmetries(
+          )
+          let cloverSymmetries = await events.Clovers.instance.getSymmetries(
             log.data._tokenId
-          );
-          var cloverBlock = await events.Clovers.instance.getBlockMinted(
+          )
+          let cloverBlock = await events.Clovers.instance.getBlockMinted(
             log.data._tokenId
-          );
+          )
+          let price = await events.SimpleCloversMarket.instance.sellPrice(
+            log.data._tokenId
+          )
           // var cloverURI = await events.Clovers.instance.tokenURI(log.data._tokenId)
         } catch (error) {
-          reject(error);
+          reject(error)
         }
         let clover = {
           name: log.data._tokenId,
@@ -31,35 +34,36 @@ export const cloversTransfer = ({ log, io, db }) => {
           reward: Number(cloverReward),
           symmetries: sym(cloverSymmetries),
           created: Number(cloverBlock),
-          modified: Number(cloverBlock)
-          // URI: cloverURI
-        };
+          modified: Number(cloverBlock),
+          // store price as hex, padded for sorting/filtering in DB
+          price: '0x' + price.replace('0x', '').padStart(64, 0)
+        }
 
         r.db("clovers_v2")
           .table("clovers")
           .insert(clover)
           .run(db, (err, result) => {
-            if (err) return reject(err);
-            io && io.emit("addClover", clover);
-            resolve();
-          });
+            if (err) return reject(err)
+            io && io.emit("addClover", clover)
+            resolve()
+          })
       } else {
         r.db("clovers_v2")
           .table("clovers")
           .get(log.data._tokenId)
           .run(db, (err, clover) => {
-            if (err) return reject(err);
+            if (err) return reject(err)
             if (!clover)
-              return reject("clover " + log.data._tokenId + " not found");
+              return reject("clover " + log.data._tokenId + " not found")
             // clover.modified = log.data.modified
-            clovers.owner = log.data._to;
+            clovers.owner = log.data._to
             r.db("clovers_v2")
               .table("clovers")
               .get(log.data._tokenId)
               .update(clover)
               .run(db, (err, result) => {
-                if (err) return reject(err);
-                io && io.emit("updateClover", clover);
+                if (err) return reject(err)
+                io && io.emit("updateClover", clover)
                 r.db("clovers_v2")
                   .table("users")
                   .get(log.data._from)
@@ -68,16 +72,16 @@ export const cloversTransfer = ({ log, io, db }) => {
                       user.clovers.splice(
                         user.clovers.indexOf(log.data._tokenId),
                         1
-                      );
+                      )
                       r.db("clovers_v2")
                         .table("users")
                         .get(log.data._to)
                         .update(user)
                         .run(db, (err, result) => {
-                          if (err) return reject(err);
-                          io && io.emit("updateUser", user);
-                          resolve();
-                        });
+                          if (err) return reject(err)
+                          io && io.emit("updateUser", user)
+                          resolve()
+                        })
                     } else {
                       // this should not happen
                       reject(
@@ -86,11 +90,11 @@ export const cloversTransfer = ({ log, io, db }) => {
                             log.data._from +
                             " but not found"
                         )
-                      );
+                      )
                     }
-                  });
-              });
-          });
+                  })
+              })
+          })
       }
     }),
     new Promise((resolve, reject) => {
@@ -99,44 +103,44 @@ export const cloversTransfer = ({ log, io, db }) => {
         .get(log.data._to)
         .run(db, (err, user) => {
           if (user) {
-            user.clovers.push(log.data._tokenId);
+            user.clovers.push(log.data._tokenId)
             r.db("clovers_v2")
               .table("users")
               .get(log.data._to)
               .update(user)
               .run(db, (err, result) => {
-                if (err) return reject(err);
-                io && io.emit("updateUser", user);
-                resolve();
-              });
+                if (err) return reject(err)
+                io && io.emit("updateUser", user)
+                resolve()
+              })
           } else {
             user = {
               name: log.data._to,
               address: log.data._to,
               clovers: [log.data._tokenId]
-            };
+            }
             r.db("clovers_v2")
               .table("users")
               .insert(user)
               .run(db, (err, result) => {
-                if (err) return reject(err);
-                io && io.emit("addUser", user);
-                resolve();
-              });
+                if (err) return reject(err)
+                io && io.emit("addUser", user)
+                resolve()
+              })
           }
-        });
+        })
     })
-  ]);
-};
+  ])
+}
 export const cloversApproval = function({ log, io, db }) {
-  console.log("NEED TO HANDLE: " + log.name);
-  return Promise.resolve();
-};
+  console.log("NEED TO HANDLE: " + log.name)
+  return Promise.resolve()
+}
 export const cloversApprovalForAll = function({ log, io, db }) {
-  console.log("NEED TO HANDLE: " + log.name);
-  return Promise.resolve();
-};
+  console.log("NEED TO HANDLE: " + log.name)
+  return Promise.resolve()
+}
 export const cloversOwnershipTransferred = function({ log, io, db }) {
-  console.log("NEED TO HANDLE: " + log.name);
-  return Promise.resolve();
-};
+  console.log("NEED TO HANDLE: " + log.name)
+  return Promise.resolve()
+}
