cd7d9a49fafcadfeab2e5537fff387492d72045f
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-08-04 22:21:28 +0200

    fixed some errors

--- src/lib/ethers-utils.js
+++ src/lib/ethers-utils.js
@@ -1,135 +1,145 @@
-import config from "../config";
+import config from '../config'
 import {
-  Clovers,
-  ClubToken,
-  CloversController,
-  SimpleCloversMarket,
-  CurationMarket
-} from "clovers-contracts";
-var ethers = Object.assign(require("ethers"), require("ethers-contracts"));
+	Clovers,
+	ClubToken,
+	CloversController,
+	SimpleCloversMarket,
+	CurationMarket
+} from 'clovers-contracts'
+var ethers = Object.assign(require('ethers'), require('ethers-contracts'))
 
-const ZeroClientProvider = require("web3-provider-engine/zero.js");
-import Web3 from "web3";
-export let iface = ethers.Interface;
-export let web3mode = false;
-ethers.apiToken = config.etherscanAPI;
-ethers.apiAccessToken = config.infuraAPI;
-var network = ethers.providers.networks.rinkeby;
-var providers = require("ethers").providers;
+const ZeroClientProvider = require('web3-provider-engine/zero.js')
+import Web3 from 'web3'
+export let iface = ethers.Interface
+export let web3mode = false
+ethers.apiToken = config.etherscanAPI
+ethers.apiAccessToken = config.infuraAPI
+var network = ethers.providers.networks.rinkeby
+var providers = require('ethers').providers
 
-var infuraProvider = new ethers.providers.InfuraProvider(network);
-var etherscanProvider = new ethers.providers.EtherscanProvider(network);
+var infuraProvider = new ethers.providers.InfuraProvider(network)
+var etherscanProvider = new ethers.providers.EtherscanProvider(network)
 
 export var jsonRpcProvider = new ethers.providers.JsonRpcProvider(
-  "http://localhost:7545",
-  ethers.providers.networks.unspecified
-);
+	'http://localhost:7545',
+	ethers.providers.networks.unspecified
+)
 
 var fallbackProvider = new ethers.providers.FallbackProvider([
-  infuraProvider,
-  etherscanProvider
-  // jsonRpcProvider
-]);
-export let provider = providers.getDefaultProvider(network);
+	infuraProvider,
+	etherscanProvider
+	// jsonRpcProvider
+])
+export let provider = providers.getDefaultProvider(network)
 // export let provider = fallbackProvider;
 // export let provider = jsonRpcProvider;
 
 var web3Provider = ZeroClientProvider({
-  getAccounts: function() {},
-  rpcUrl: "https://rinkeby.infura.io/v3/" + config.infuraKey
-});
-export var web3 = new Web3(web3Provider);
+	getAccounts: function() {},
+	rpcUrl: 'https://rinkeby.infura.io/v3/' + config.infuraKey
+})
+export var web3 = new Web3(web3Provider)
 
-let simpleCloversMarketABI = SimpleCloversMarket.abi;
+let simpleCloversMarketABI = SimpleCloversMarket.abi
 let simpleCloversMarketAddress =
-  SimpleCloversMarket.networks[config.networkId].address;
+	SimpleCloversMarket.networks[config.networkId].address
 let simpleCloversMarketInstance = new ethers.Contract(
-  simpleCloversMarketAddress,
-  simpleCloversMarketABI,
-  provider
-);
+	simpleCloversMarketAddress,
+	simpleCloversMarketABI,
+	provider
+)
 
-let curationMarketABI = CurationMarket.abi;
-let curationMarketAddress = CurationMarket.networks[config.networkId].address;
+let curationMarketABI = CurationMarket.abi
+let curationMarketAddress = CurationMarket.networks[config.networkId].address
 let curationMarketInstance = new ethers.Contract(
-  curationMarketAddress,
-  curationMarketABI,
-  provider
-);
+	curationMarketAddress,
+	curationMarketABI,
+	provider
+)
 
-let cloversABI = Clovers.abi;
-let cloversAddress = Clovers.networks[config.networkId].address;
-let cloversInstance = new ethers.Contract(cloversAddress, cloversABI, provider);
-let _clovers = web3.eth.contract(cloversABI);
-let cloversWeb3Instance = _clovers.at(cloversAddress);
+let cloversABI = Clovers.abi
+let cloversAddress = Clovers.networks[config.networkId].address
+let cloversInstance = new ethers.Contract(cloversAddress, cloversABI, provider)
+let _clovers = web3.eth.contract(cloversABI)
+let cloversWeb3Instance = _clovers.at(cloversAddress)
 
-let clubTokenABI = ClubToken.abi;
-let clubTokenAddress = ClubToken.networks[config.networkId].address;
+let clubTokenABI = ClubToken.abi
+let clubTokenAddress = ClubToken.networks[config.networkId].address
 let clubTokenInstance = new ethers.Contract(
-  clubTokenAddress,
-  clubTokenABI,
-  provider
-);
-let _clubToken = web3.eth.contract(clubTokenABI);
-let clubTokenWeb3Instance = _clubToken.at(clubTokenAddress);
+	clubTokenAddress,
+	clubTokenABI,
+	provider
+)
+let _clubToken = web3.eth.contract(clubTokenABI)
+let clubTokenWeb3Instance = _clubToken.at(clubTokenAddress)
 
-let cloversControllerABI = CloversController.abi;
+let cloversControllerABI = CloversController.abi
 let cloversControllerAddress =
-  CloversController.networks[config.networkId].address;
+	CloversController.networks[config.networkId].address
 let cloversControllerInstance = new ethers.Contract(
-  cloversControllerAddress,
-  cloversControllerABI,
-  provider
-);
-let _cloversController = web3.eth.contract(cloversControllerABI);
+	cloversControllerAddress,
+	cloversControllerABI,
+	provider
+)
+let _cloversController = web3.eth.contract(cloversControllerABI)
 let cloversControllerWeb3Instance = _cloversController.at(
-  cloversControllerAddress
-);
+	cloversControllerAddress
+)
+
+const walletProvider = new ethers.Wallet(config.oraclePrivateKey, provider)
+
+export let wallet = {
+	CloversController: new ethers.Contract(
+		cloversControllerAddress,
+		cloversControllerABI,
+		walletProvider
+	)
+}
 
 export let events = {
-  SimpleCloversMarket: {
-    abi: simpleCloversMarketABI,
-    address: simpleCloversMarketAddress,
-    instance: simpleCloversMarketInstance,
-    eventTypes: ["updatePrice", "OwnershipTransferred"]
-  },
-  CurationMarket: {
-    abi: curationMarketABI,
-    address: curationMarketAddress,
-    instance: curationMarketInstance,
-    eventTypes: ["Transfer", "Mint", "Burn", "OwnershipTransferred"]
-  },
-  Clovers: {
-    abi: cloversABI,
-    address: cloversAddress,
-    instance: cloversInstance,
-    // web3instance: cloversWeb3Instance,
-    eventTypes: [
-      "Transfer",
-      "Approval",
-      "ApprovalForAll",
-      "OwnershipTransferred"
-    ]
-  },
-  ClubToken: {
-    abi: clubTokenABI,
-    address: clubTokenAddress,
-    instance: clubTokenInstance,
-    // web3instance: clubTokenWeb3Instance,
-    eventTypes: ["Burn", "Mint", "Approval", "Transfer", "OwnershipTransferred"]
-  },
-  CloversController: {
-    abi: cloversControllerABI,
-    address: cloversControllerAddress,
-    instance: cloversControllerInstance,
-    // web3instance: cloversControllerWeb3Instance,
-    eventTypes: [
-      "cloverCommitted",
-      "cloverClaimed",
-      // "stakeAndRewardRetrieved",
-      // "cloverChallenged",
-      // "stakeRetrieved",
-      "OwnershipTransferred"
-    ]
-  }
-};
+	SimpleCloversMarket: {
+		abi: simpleCloversMarketABI,
+		address: simpleCloversMarketAddress,
+		instance: simpleCloversMarketInstance,
+		eventTypes: ['updatePrice', 'OwnershipTransferred']
+	},
+	CurationMarket: {
+		abi: curationMarketABI,
+		address: curationMarketAddress,
+		instance: curationMarketInstance,
+		eventTypes: ['Transfer', 'Mint', 'Burn', 'OwnershipTransferred']
+	},
+	Clovers: {
+		abi: cloversABI,
+		address: cloversAddress,
+		instance: cloversInstance,
+		// web3instance: cloversWeb3Instance,
+		eventTypes: [
+			'Transfer',
+			'Approval',
+			'ApprovalForAll',
+			'OwnershipTransferred'
+		]
+	},
+	ClubToken: {
+		abi: clubTokenABI,
+		address: clubTokenAddress,
+		instance: clubTokenInstance,
+		// web3instance: clubTokenWeb3Instance,
+		eventTypes: ['Burn', 'Mint', 'Approval', 'Transfer', 'OwnershipTransferred']
+	},
+	CloversController: {
+		abi: cloversControllerABI,
+		address: cloversControllerAddress,
+		instance: cloversControllerInstance,
+		// web3instance: cloversControllerWeb3Instance,
+		eventTypes: [
+			'cloverCommitted',
+			'cloverClaimed',
+			// "stakeAndRewardRetrieved",
+			// "cloverChallenged",
+			// "stakeRetrieved",
+			'OwnershipTransferred'
+		]
+	}
+}
--- src/lib/util.js
+++ src/lib/util.js
@@ -1,201 +1,200 @@
-/** Creates a callback that proxies node callback style arguments to an Express Response object.
- *  @param {express.Response} res Express HTTP Response
- *  @param {number} [status=200]  Status code to send on success
- *
- *  @example
- *    list(req, res) {
- *      collection.find({}, toRes(res))
- *    }
- */
+import Reversi from 'clovers-reversi'
+import svg_to_png from 'svg-to-png'
+import fs from 'fs-extra'
+import path from 'path'
+import BigNumber from 'bignumber.js'
+var utils = require('ethers').utils
 
-import Reversi from "clovers-reversi";
-import svg_to_png from "svg-to-png";
-import fs from "fs-extra";
-import path from "path";
-import BigNumber from "bignumber.js";
-import utils from "web3-utils";
+export const oneEthInWei = utils.parseEther('1').toString(10)
 
-export const oneEthInWei = utils.toWei("1");
+export function dodb(db, command) {
+	return new Promise((resolve, reject) => {
+		command.run(db, (err, result) => {
+			if (err) return reject(err)
+			resolve(result)
+		})
+	})
+}
 
 export async function getLowestPrice(
-  contract,
-  targetAmount,
-  _tokenId = null,
-  currentPrice = new BigNumber("0"),
-  useLittle = false
+	contract,
+	targetAmount,
+	_tokenId = null,
+	currentPrice = new BigNumber('0'),
+	useLittle = false
 ) {
-  if (typeof targetAmount !== "object") {
-    targetAmount = new BigNumber(targetAmount);
-  }
-  let littleIncrement = new BigNumber(utils.toWei("0.001"));
-  let bigIncrement = new BigNumber(utils.toWei("0.1"));
-  currentPrice = currentPrice.add(useLittle ? littleIncrement : bigIncrement);
-  if (_tokenId) {
-    let resultOfSpend = await contract.getBuy(_tokenId, currentPrice);
-  } else {
-    let resultOfSpend = await contract.getBuy(currentPrice);
-  }
-  if (resultOfSpend.gt(targetAmount)) {
-    return useLittle
-      ? currentPrice
-      : getLowestPrice(
-          contract,
-          targetAmount,
-          _tokenId,
-          currentPrice.sub(bigIncrement),
-          true
-        );
-  }
-  return getLowestPrice(
-    contract,
-    targetAmount,
-    _tokenId,
-    currentPrice,
-    useLittle
-  );
+	if (typeof targetAmount !== 'object') {
+		targetAmount = new BigNumber(targetAmount)
+	}
+	let littleIncrement = utils.parseEther('0.001')
+	let bigIncrement = utils.parseEther('0.1')
+	currentPrice = currentPrice.add(useLittle ? littleIncrement : bigIncrement)
+	if (_tokenId) {
+		let resultOfSpend = await contract.getBuy(_tokenId, currentPrice)
+	} else {
+		let resultOfSpend = await contract.getBuy(currentPrice)
+	}
+	if (resultOfSpend.gt(targetAmount)) {
+		return useLittle
+			? currentPrice
+			: getLowestPrice(
+					contract,
+					targetAmount,
+					_tokenId,
+					currentPrice.sub(bigIncrement),
+					true
+			  )
+	}
+	return getLowestPrice(
+		contract,
+		targetAmount,
+		_tokenId,
+		currentPrice,
+		useLittle
+	)
 }
 
 export function parseLogForStorage(l) {
-  Object.keys(l).map((key, index) => {
-    if (l[key]._bn) {
-      if (key === "_tokenId") {
-        l[key] = "0x" + l[key]._bn.toString(16);
-      } else {
-        l[key] = padBigNum(l[key]._bn.toString(16));
-      }
-    }
-  });
-  return l;
+	Object.keys(l).map((key, index) => {
+		if (l[key]._bn) {
+			if (key === '_tokenId') {
+				l[key] = '0x' + l[key]._bn.toString(16)
+			} else {
+				l[key] = padBigNum(l[key]._bn.toString(16))
+			}
+		}
+	})
+	return l
 }
 
 export function padBigNum(amount) {
-  if (typeof amount === "object") amount = amount.toString(16);
-  return "0x" + amount.replace("0x", "").padStart(64, 0);
+	if (typeof amount === 'object') amount = amount.toString(16)
+	return '0x' + amount.replace('0x', '').padStart(64, 0)
 }
 
 export function toRes(res, status = 200) {
-  return (err, thing) => {
-    if (err) return res.status(500).send(err);
+	return (err, thing) => {
+		if (err) return res.status(500).send(err)
 
-    if (thing && typeof thing.toObject === "function") {
-      thing = thing.toObject();
-    }
-    thing
-      .toArray()
-      .then(results => {
-        res.status(status).json(results);
-      })
-      .error(console.log);
-  };
+		if (thing && typeof thing.toObject === 'function') {
+			thing = thing.toObject()
+		}
+		thing
+			.toArray()
+			.then(results => {
+				res.status(status).json(results)
+			})
+			.error(console.log)
+	}
 }
 
 export function toSVG(id, size = 400) {
-  size = parseInt(size);
-  return new Promise((resolve, reject) => {
-    let green = "#008B39";
-    let black = "#000000";
-    let white = "#FFFFFF";
-    let grey = "#808080";
+	size = parseInt(size)
+	return new Promise((resolve, reject) => {
+		let green = '#008B39'
+		let black = '#000000'
+		let white = '#FFFFFF'
+		let grey = '#808080'
 
-    let r = new Reversi();
-    let svgPath = path.resolve(
-      __dirname + "/../../public/svg/" + size + "/" + id + ".svg"
-    );
+		let r = new Reversi()
+		let svgPath = path.resolve(
+			__dirname + '/../../public/svg/' + size + '/' + id + '.svg'
+		)
 
-    r.byteBoardPopulateBoard(id);
-    r.calcWinners();
+		r.byteBoardPopulateBoard(id)
+		r.calcWinners()
 
-    let fill, stroke, sequence;
-    let strokeWidth = 1;
-    let radius = size / 2;
+		let fill, stroke, sequence
+		let strokeWidth = 1
+		let radius = size / 2
 
-    let svg =
-      '<?xml version="1.0" encoding="UTF-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="' +
-      size +
-      'px" height="' +
-      size +
-      'px" viewBox="-1 -1 ' +
-      (size + 2) +
-      " " +
-      (size + 2) +
-      '" enable-background="new 0 0 ' +
-      size +
-      " " +
-      size +
-      '" xml:space="preserve">';
+		let svg =
+			'<?xml version="1.0" encoding="UTF-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="' +
+			size +
+			'px" height="' +
+			size +
+			'px" viewBox="-1 -1 ' +
+			(size + 2) +
+			' ' +
+			(size + 2) +
+			'" enable-background="new 0 0 ' +
+			size +
+			' ' +
+			size +
+			'" xml:space="preserve">'
 
-    if (r.whiteScore < r.blackScore) {
-      fill = black;
-      stroke = black;
-    } else if (r.whiteScore > r.blackScore) {
-      fill = white;
-      stroke = black;
-    } else {
-      fill = grey;
-      stroke = grey;
-    }
+		if (r.whiteScore < r.blackScore) {
+			fill = black
+			stroke = black
+		} else if (r.whiteScore > r.blackScore) {
+			fill = white
+			stroke = black
+		} else {
+			fill = grey
+			stroke = grey
+		}
 
-    svg +=
-      '<circle shape-rendering="optimizeQuality" fill="' +
-      fill +
-      '" stroke="' +
-      stroke +
-      '" stroke-width="' +
-      strokeWidth +
-      '" stroke-miterlimit="10" cx="' +
-      size / 2 +
-      '" cy="' +
-      size / 2 +
-      '" r="' +
-      radius +
-      '"/>';
+		svg +=
+			'<circle shape-rendering="optimizeQuality" fill="' +
+			fill +
+			'" stroke="' +
+			stroke +
+			'" stroke-width="' +
+			strokeWidth +
+			'" stroke-miterlimit="10" cx="' +
+			size / 2 +
+			'" cy="' +
+			size / 2 +
+			'" r="' +
+			radius +
+			'"/>'
 
-    for (let i = 0; i < 64; i++) {
-      let row = Math.floor(i / 8);
-      let col = i % 8;
-      switch (r.board[row][col]) {
-        case r.BLACK:
-          if (r.whiteScore < r.blackScore) continue;
-          fill = black;
-          stroke = "none";
-          break;
-        case r.WHITE:
-          if (r.whiteScore > r.blackScore) continue;
-          fill = white;
-          stroke = "none";
-          break;
-        case r.EMPTY:
-          fill = green;
-          stroke = "none";
-        default:
-          continue;
-      }
-      let x = (row + 1) * (size / 12) + size / 8;
-      let y = (col + 1) * (size / 12) + size / 8;
-      svg +=
-        '<circle shape-rendering="optimizeQuality" fill="' +
-        fill +
-        '" stroke="' +
-        stroke +
-        '" stroke-miterlimit="1" cx="' +
-        x +
-        '" cy="' +
-        y +
-        '" r="' +
-        size / 24 +
-        '"/>';
-    }
-    svg += "</svg>";
-    resolve(svg);
+		for (let i = 0; i < 64; i++) {
+			let row = Math.floor(i / 8)
+			let col = i % 8
+			switch (r.board[row][col]) {
+				case r.BLACK:
+					if (r.whiteScore < r.blackScore) continue
+					fill = black
+					stroke = 'none'
+					break
+				case r.WHITE:
+					if (r.whiteScore > r.blackScore) continue
+					fill = white
+					stroke = 'none'
+					break
+				case r.EMPTY:
+					fill = green
+					stroke = 'none'
+				default:
+					continue
+			}
+			let x = (row + 1) * (size / 12) + size / 8
+			let y = (col + 1) * (size / 12) + size / 8
+			svg +=
+				'<circle shape-rendering="optimizeQuality" fill="' +
+				fill +
+				'" stroke="' +
+				stroke +
+				'" stroke-miterlimit="1" cx="' +
+				x +
+				'" cy="' +
+				y +
+				'" r="' +
+				size / 24 +
+				'"/>'
+		}
+		svg += '</svg>'
+		resolve(svg)
 
-    // fs.outputFile(svgPath, svg, (err) => {
-    //   if (err) {
-    //     reject(err)
-    //   } else {
-    //     resolve()
-    //   }
-    // })
-  });
+		// fs.outputFile(svgPath, svg, (err) => {
+		//   if (err) {
+		//     reject(err)
+		//   } else {
+		//     resolve()
+		//   }
+		// })
+	})
 }
 
 // export function toPNG (id) {
@@ -216,16 +215,16 @@ export function toSVG(id, size = 400) {
 // }
 
 export function sym(syms) {
-  let RotSym = (syms >> 4) & (1 == 1);
-  let Y0Sym = (syms >> 3) & (1 == 1);
-  let X0Sym = (syms >> 2) & (1 == 1);
-  let XYSym = (syms >> 1) & (1 == 1);
-  let XnYSym = syms & (1 == 1);
-  return {
-    RotSym,
-    Y0Sym,
-    X0Sym,
-    XYSym,
-    XnYSym
-  };
+	let RotSym = (syms >> 4) & (1 == 1)
+	let Y0Sym = (syms >> 3) & (1 == 1)
+	let X0Sym = (syms >> 2) & (1 == 1)
+	let XYSym = (syms >> 1) & (1 == 1)
+	let XnYSym = syms & (1 == 1)
+	return {
+		RotSym,
+		Y0Sym,
+		X0Sym,
+		XYSym,
+		XnYSym
+	}
 }
