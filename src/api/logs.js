0867b2addbff1b6f08ed24162507976ae06728a3
N Kort <nics@limnerstudio.co.uk>
2019-01-27 18:45:44 +0100

    routes for activity

--- src/api/logs.js
+++ src/api/logs.js
@@ -5,12 +5,8 @@ import { toRes } from '../lib/util'
 
 export default ({ config, db, io }) =>
   resource({
-    /** Property name to store preloaded entity on `request`. */
     id: 'log',
 
-    /** For requests with an `id`, you can auto-load the entity.
-     *  Errors terminate the request, success sets `req[id] = data`.
-     */
     load(req, id, callback) {
       r.db('clovers_v2')
         .table('logs')
@@ -20,40 +16,36 @@ export default ({ config, db, io }) =>
         })
     },
 
-    /** GET / - List all entities */
     index({ query }, res) {
-      let limit = parseInt(query.limit) || 100
+      let limit = parseInt(query.limit) || 50
       let offset = parseInt(query.offset) || 0
-      limit = Math.min(limit, 500)
+      limit = Math.min(limit, 150)
       r.db('clovers_v2')
         .table('logs')
+        .getAll('pub', { index: 'activity' })
+        .orderBy(r.desc('blockNumber'))
         .slice(offset, offset + limit)
         .run(db, toRes(res))
     },
 
-    /** POST / - Create a new entity */
-    create({ body }, res) {
-      res.json(body)
-    },
+    // create({ body }, res) {
+    //   res.json(body)
+    // },
 
-    /** GET /:id - Return a given entity */
     read({ log }, res) {
       res.json(log)
     },
 
-    /** PUT /:id - Update a given entity */
-    update({ log, body }, res) {
-      for (let key in body) {
-        if (key !== 'id') {
-          log[key] = body[key]
-        }
-      }
-      res.sendStatus(204)
-    },
+    // update({ log, body }, res) {
+    //   for (let key in body) {
+    //     if (key !== 'id') {
+    //       log[key] = body[key]
+    //     }
+    //   }
+    //   res.sendStatus(204)
+    // },
 
-    /** DELETE /:id - Delete a given entity */
-    delete({ log }, res) {
-      // logs.splice(logs.indexOf(log), 1)
-      res.sendStatus(204)
-    }
+    // delete({ log }, res) {
+    //   res.sendStatus(204)
+    // }
   })
