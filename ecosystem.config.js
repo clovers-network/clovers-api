5ef647b2515497966d82677740c9ec90bc723771
N Kort <nics@limnerstudio.co.uk>
2019-08-04 16:41:44 +0200

    update pm2 settings

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps : [{
+    name: 'API',
+    script: 'dist/index.js',
+    env: {
+      NODE_ENV: 'development',
+      DEBUG: 'app:*'
+    },
+    env_production : {
+      NODE_ENV: 'production',
+      DEBUG: 'app:*'
+    }
+  }]
+};
