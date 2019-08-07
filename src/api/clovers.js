745f350893c00fb366a88f65d3b587370c0681bf
N Kort <nics@limnerstudio.co.uk>
2019-08-07 14:15:24 +0200

    update allowed indexes for clovers

--- src/api/clovers.js
+++ src/api/clovers.js
@@ -53,7 +53,7 @@ export default ({ config, db, io }) => {
     load,
 
     async index({ query }, res) {
-      const indexes = ['all', 'forsale', 'rft', 'RotSym', 'X0Sym', 'Y0Sym', 'XYSym', 'XnYSym', 'Sym', 'public']
+      const indexes = ['all', 'forsale', 'rft', 'RotSym', 'X0Sym', 'Y0Sym', 'XYSym', 'XnYSym', 'Sym', 'public', 'contract']
       const pageSize = 12
       const sort = query.sort || 'modified'
       const asc = query.asc === 'true'
