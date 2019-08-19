5a66278e2a4ccdc9f3e562e06e48b34b73282e73
okwme <billy.rennekamp@gmail.com>
2019-08-20 00:55:35 +0200

    increase gas again

--- src/models/clovers.js
+++ src/models/clovers.js
@@ -311,7 +311,7 @@ async function addNewClover(log) {
 async function oracleVerify ({ name, moves }, symmetries) {
   debug(name + ' is being verified')
   const options = {
-    gasPrice: 5000000000 // 5 GWEI
+    gasPrice: 10000000000 // 10 GWEI
   }
   try {
     // dont verify clovers from the initial build
