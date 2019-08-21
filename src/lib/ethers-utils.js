9a4581781f2d864abba51d6fda4cc2cec7b9207b
okwme <billy.rennekamp@gmail.com>
2019-08-22 00:52:59 +0200

    update infura

--- src/lib/ethers-utils.js
+++ src/lib/ethers-utils.js
@@ -23,16 +23,16 @@ var infuraProvider = new ethers.providers.InfuraProvider(network)
 var etherscanProvider = new ethers.providers.EtherscanProvider(network)
 
 export var jsonRpcProvider = new ethers.providers.JsonRpcProvider(
-  'http://mainnet.infura.io/v3/e5b8088fe4df4a69a4b5f35c2561bbe0',
-  ethers.providers.networks.unspecified
+  'https://mainnet.infura.io/v3/e5b8088fe4df4a69a4b5f35c2561bbe0',
+  network
 )
 
 var fallbackProvider = new ethers.providers.FallbackProvider([
+  jsonRpcProvider,
   infuraProvider,
   etherscanProvider
-  // jsonRpcProvider
 ])
-export let provider = providers.getDefaultProvider(network)
+export let provider = jsonRpcProvider // providers.getDefaultProvider(network)
 // export let provider = fallbackProvider;
 // export let provider = jsonRpcProvider;
 
