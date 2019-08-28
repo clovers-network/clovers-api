2c7f7d2471bdc23faf9a89825c82e1e729439416
okwme <billy.rennekamp@gmail.com>
2019-08-28 18:51:51 +0200

    fix

--- src/models/clovers.js
+++ src/models/clovers.js
@@ -510,7 +510,7 @@ async function oracleVerify (clover, symmetries) {
   try {
     console.log(fast.toString(16), fast.toString(10), fast.toHexString())
     const options = {
-      gasPrice: fast.toString(10)
+      gasPrice: fast.toHexString()
     }
     console.log({fast: fast.toString(10), gasPriceEth: formatEther(fast)})
     // dont verify clovers from the initial build
